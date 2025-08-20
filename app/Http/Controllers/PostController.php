9142ef921ff54cef7da7754a652fb5618256e162
Paul Lourence Calicoy <paullourencecalicoy@gmail.com>
2025-08-20 17:08:50 +0800

    Latest

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Support\Facades\Gate;
+
+class PostController extends Controller implements HasMiddleware
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public static function middleware()
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show'])
+        ];
+    }
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'Username' => 'required|string|max:255|unique:posts',
+            'Caption' => 'required|string|max:255',
+            'Post' => 'required|string',
+            'Comment' => 'nullable|string',
+        ]);
+
+        $post = $request->user()->posts()->create($fields);
+
+        return ['post' => $post];
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        Gate::authorize('modify', $post);
+        
+         $fields = $request->validate([
+            'Caption' => 'required|string|max:255',
+            'Post' => 'required|string',
+            'comment' => 'nullable|string',
+        ]);
+
+        $post->update($fields);
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        
+
+        $post->delete();
+
+        return ['message' => 'The post was deleted'];
+    }
+}
